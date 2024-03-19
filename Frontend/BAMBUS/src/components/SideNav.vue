9628d9701d3959b8bff897c2783a894bfb14489d
saltyniggo <n.suraimu@proton.me>
2024-03-19 15:24:01 +0100

    Refactor SideNav component

--- Frontend/BAMBUS/src/components/SideNav.vue
+++ Frontend/BAMBUS/src/components/SideNav.vue
@@ -1,7 +1,30 @@
 <template>
-  <div>
-    <h3>asdljhads</h3>
+  <div class="SideNav">
+    <section class="SideNav-Item-Link-Tree">
+      <p>Toller Buch Bums</p>
+      <p>Toller Magazin Bums</p>
+      <p>Toller Spiel Bums</p>
+    </section>
+    <section class="SideNav-User-Area">
+      <p>User Specific Bums</p>
+      <p>Login</p>
+    </section>
   </div>
 </template>
 
-<script></script>
+<script>
+export default {
+  name: "SideNav",
+};
+</script>
+
+<style scoped>
+.SideNav {
+}
+
+.SideNav-Item-Link-Tree {
+}
+
+.SideNav-User-Area {
+}
+</style>
