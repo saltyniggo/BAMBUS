4270bc6cb211fbdd16d7ff7ed31a592aadf0989b
saltyniggo <n.suraimu@proton.me>
2024-03-19 15:15:21 +0100

    Delete unnecessary files and update dependencies

--- Frontend/BAMBUS/src/router/index.js
+++ Frontend/BAMBUS/src/router/index.js
@@ -1,23 +1,38 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory } from "vue-router";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: HomeView
+      path: "/",
+      name: "catalog",
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
-  ]
-})
+      path: "/cart",
+      name: "cart",
+    },
+    {
+      path: "/my-view",
+      name: "loaner-view",
+    },
+    {
+      path: "/admin",
+      name: "admin-view",
+    },
+    {
+      path: "/managing",
+      name: "manager-view",
+    },
+    {
+      path: "/login",
+      name: "login",
+    },
+    {
+      path: "/*",
+      name: "not-found",
+      redirect: { name: "catalog" },
+    },
+  ],
+});
 
-export default router
+export default router;
