ab9515f7a68c1793f61a9acfee0bfc310c105154
saltyniggo <n.suraimu@proton.me>
2024-03-22 09:38:09 +0100

    case sensitive lol

--- Frontend/BAMBUS/src/components/catalog-page/CatalogPage.vue
+++ Frontend/BAMBUS/src/components/catalog-page/CatalogPage.vue
@@ -1,77 +1,84 @@
 <template>
-     <div class="catalog" id="catalog">
+  <div class="catalog" id="catalog">
+    <CatalogFilter></CatalogFilter>
 
-        <CatalogFilter></CatalogFilter>
-
-        <div class="catalog-items" v-if="items">
-            <item-container v-for="item in items" :key="item.id" :item="item" @openEditModal="openEditModal" @openRatingsModal="openRatingsModal"></item-container>
-        </div>
-        <EditModal v-if="showsEditModal"></EditModal>
-        <RatingsModal v-if="showsRatingsModal"></RatingsModal>
-
+    <div class="catalog-items" v-if="items">
+      <item-container
+        v-for="item in items"
+        :key="item.id"
+        :item="item"
+        @openEditModal="openEditModal"
+        @openRatingsModal="openRatingsModal"
+      ></item-container>
     </div>
+    <EditModal v-if="showsEditModal"></EditModal>
+    <RatingsModal v-if="showsRatingsModal"></RatingsModal>
+  </div>
 </template>
 
 <script>
-import CatalogFilter from './CatalogFilter.vue';
+import CatalogFilter from "./CatalogFilter.vue";
 import itemContainer from "../base-components/base-item-container.vue";
 import EditModal from "./EditItemModal.vue";
 import RatingsModal from "./RatingsModal.vue";
 import { mapGetters } from "vuex";
 
 export default {
-    name: "CatalogPage",
-    components: {
-        CatalogFilter,
-        itemContainer,
-        EditModal,
-        RatingsModal,
+  name: "CatalogPage",
+  components: {
+    CatalogFilter,
+    itemContainer,
+    EditModal,
+    RatingsModal,
+  },
+  props: {
+    category: {
+      type: String,
+      default: "Alle",
     },
-    props: {
-        category: {
-            type: String,
-            default: "Alle"
-        }
-    },
+  },
 
-    computed:{
-        ...mapGetters({items: "itemStore/getItems", showsEditModal: "modalStore/getEditModalStatus", showsRatingsModal: "modalStore/getRatingsModalStatus"}),
+  computed: {
+    ...mapGetters({
+      items: "itemStore/getItems",
+      showsEditModal: "modalStore/getEditModalStatus",
+      showsRatingsModal: "modalStore/getRatingsModalStatus",
+    }),
+  },
+  watch: {
+    category() {
+      console.log("Category changed to: " + this.category);
+      console.log("Items: " + this.items);
     },
-    watch: {
-        category() {
-            console.log("Category changed to: " + this.category);
-            console.log("Items: " + this.items);
-        }
+  },
+  methods: {
+    openEditModal(id) {
+      this.$store.dispatch("modalStore/closeAllModals");
+      this.$store.dispatch("itemStore/setEditItemId", id);
+      this.$store.dispatch("modalStore/toggleEditModal");
     },
-    methods: {
-        openEditModal(id) {
-            this.$store.dispatch("modalStore/closeAllModals");
-            this.$store.dispatch("ItemStore/setEditItemId", id);
-            this.$store.dispatch("modalStore/toggleEditModal");
-        },
-        openRatingsModal(id) {
-            this.$store.dispatch("modalStore/closeAllModals");
-            this.$store.dispatch("itemStore/setRatingsId", id);
-            this.$store.dispatch("modalStore/toggleRatingsModal");
-        },
+    openRatingsModal(id) {
+      this.$store.dispatch("modalStore/closeAllModals");
+      this.$store.dispatch("itemStore/setRatingsId", id);
+      this.$store.dispatch("modalStore/toggleRatingsModal");
     },
+  },
 };
-
 </script>
 
 <style scoped>
-   .catalog {
-        min-height: 100vh;
-        width: 100%;
-        background-color: #3b8c84;
-    }
+.catalog {
+  min-height: 100vh;
+  width: 100%;
+  background-color: #3b8c84;
+}
 
-    .catalog-items {
-        display: grid;
-        grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
-        grid-gap: 2rem;
-        padding: 2rem;
-        align-items: stretch;
-        justify-items: center;
-    }
+.catalog-items {
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
+  grid-gap: 2rem;
+  padding: 2rem;
+  align-items: stretch;
+  justify-items: center;
+}
 </style>
