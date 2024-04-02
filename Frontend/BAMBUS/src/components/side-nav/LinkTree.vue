5d569a1f403f3e8df1d94483c7198143419fcf91
saltyniggo <n.suraimu@proton.me>
2024-04-02 12:48:27 +0200

    danke vue

--- Frontend/BAMBUS/src/components/side-nav/LinkTree.vue
+++ Frontend/BAMBUS/src/components/side-nav/LinkTree.vue
@@ -1,12 +1,40 @@
 <template>
   <div class="SideNav-Item-Link-Tree">
-    <router-link :to="{ name: 'catalog-category', params: { category: 'all' }, hash: '#catalog' }" >Toller Alles Bums</router-link>
+    <router-link
+      :to="{
+        name: 'catalog-category',
+        params: { category: 'all' },
+        hash: '#catalog',
+      }"
+      >Toller Alles Bums</router-link
+    >
     <br />
-    <router-link :to="{ name: 'catalog-category', params: { category: 'books' }, hash: '#catalog' }" >Toller Buch Bums</router-link>
+    <router-link
+      :to="{
+        name: 'catalog-category',
+        params: { category: 'books' },
+        hash: '#catalog',
+      }"
+      >Toller Buch Bums</router-link
+    >
     <br />
-    <router-link :to="{ name: 'catalog-category', params: { category: 'magazines' }, hash: '#catalog' }" >Toller Magazin Bums</router-link>
+    <router-link
+      :to="{
+        name: 'catalog-category',
+        params: { category: 'magazines' },
+        hash: '#catalog',
+      }"
+      >Toller Magazin Bums</router-link
+    >
     <br />
-    <router-link :to="{ name: 'catalog-category', params: { category: 'games' }, hash: '#catalog' }" >Toller Spiele Bums</router-link>
+    <router-link
+      :to="{
+        name: 'catalog-category',
+        params: { category: 'games' },
+        hash: '#catalog',
+      }"
+      >Toller Spiele Bums</router-link
+    >
   </div>
 </template>
 
