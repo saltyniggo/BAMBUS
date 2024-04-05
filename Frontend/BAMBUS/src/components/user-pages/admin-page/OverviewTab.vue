38307e3fc29b6e55b077069839a3570b97f1fde4
saltyniggo <n.suraimu@proton.me>
2024-04-05 08:57:25 +0200

    added the refresh button to the manager and admin page

--- Frontend/BAMBUS/src/components/user-pages/admin-page/OverviewTab.vue
+++ Frontend/BAMBUS/src/components/user-pages/admin-page/OverviewTab.vue
@@ -1,6 +1,13 @@
 <template>
   <div>
     <h1>Benachrichtigungen</h1>
+    <button
+      class="notification-refresh-button"
+      @click="console.log('hallo oida')"
+    >
+      <i class="fa-solid fa-arrows-rotate"></i>
+      <p>Neuladen</p>
+    </button>
     <base-notification
       v-for="notification in user.notifications"
       :key="notification.id"
@@ -25,4 +32,28 @@ export default {
 };
 </script>
 
-<style></style>
+<style scoped>
+.notification-refresh-button {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin: 0.5rem 0;
+  padding: 0.5rem;
+  background-color: #2d2d2d;
+  color: white;
+  border: none;
+  border-radius: 0.5rem;
+  cursor: pointer;
+}
+
+.notification-refresh-button:hover {
+  background-color: #3d3d3d;
+  box-shadow: 0 0 0.25rem rgba(0, 0, 0, 0.5);
+}
+
+.notification-refresh-button i,
+p {
+  padding: 0 0.25rem;
+  color: #fff;
+}
+</style>
