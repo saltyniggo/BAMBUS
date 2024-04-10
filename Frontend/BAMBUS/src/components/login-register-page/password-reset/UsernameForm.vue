929e98f543349ae8f0e7252e4564197ef88103cd
Frieda <heyjoo.3@gmail.com>
2024-04-10 11:37:40 +0200

    Backup, weil VM spinnt

--- Frontend/BAMBUS/src/components/login-register-page/password-reset/UsernameForm.vue
+++ Frontend/BAMBUS/src/components/login-register-page/password-reset/UsernameForm.vue
@@ -1,22 +1,22 @@
 <template>
-  <form @submit.prevent class="email-form">
+  <form   class="email-form">
     <label for="email"
       >Bitte gebe Deine Emailadresse ein, um eine Passwortzurücksetzung zu benatragen.
     </label>
-    <input
-      class="email-form-input"
-      type="text"
-      placeholder="Emailadresse"
-      v-model="email"
-    />
-    <base-text-button @click="submitPasswordReset()" >
+    <Form v-slot="{validate}" @submit.prevent="onSubmit">
+      <Field name="email" type=email :rules="validateEmail" class="email-form-input" v-model="email" placeholder="Emailadresse"/>
+      <ErrorMessage name="email" />
+    <base-text-button>
       Passwort zurücksetzen
     </base-text-button>
+  </Form>
   </form>
 </template>
 
 <script>
 import { mapActions } from "vuex";
+import {Form, Field, ErrorMessage} from "vee-validate";
+import { required, email } from "@vee-validate/rules";
 
 import BaseTextButton from "../../base-components/BaseTextButton.vue";
 
@@ -24,6 +24,9 @@ export default {
   name: "PasswordResetPage",
   components: {
     BaseTextButton,
+    Form, 
+    Field, 
+    ErrorMessage
   },
   data() {
     return {
@@ -31,27 +34,43 @@ export default {
     };
   },
   methods: {
-    submitPasswordReset() {
-      this.requestPasswordReset(this.email);
-    },
     ...mapActions("notificationStore", {
       requestPasswordReset: "userRequestsPasswordReset",
     }),
+    onSubmit(values) {
+      console.log("Form submitted", values);
+      this.$refs.Form.validate('email').then((success) => {
+        if (success) {
+          console.log("Form submitted", values);
+          this.requestPasswordReset(values.email);
+        }
+      });
+    },
+    validateEmail(value) {
+      if (!value) {
+        return 'Dieses Feld muss ausgefüllt werden';
+      }
+      const regex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i;
+      if (!regex.test(value)) {
+        return 'Bitte gebe eine gültige Emailadresse an';
+      }
+      return true;
+    },
   },
 };
 </script>
 
 <style scoped>
-.username-form {
+.email-form, Form {
   width: 100%;
   display: flex;
   flex-direction: column;
-  justify-content: space-between;
+  justify-content: center;
   align-items: center;
 }
 
-.username-form-input {
-  width: 45%;
+.email-form-input {
+  width: 80%;
   margin: 2.5%;
   padding: 2%;
   border-radius: 0.5rem;
@@ -59,18 +78,23 @@ export default {
   box-shadow: 0px 0px 5px 0px #222126;
 }
 
-.username-form-input:focus {
+.email-form-input:focus {
   box-shadow: 0px 0px 10px 0px #222126;
 }
 
-.username-form-input:active {
+.email-form-input:active {
   box-shadow: 0px 0px 5px 0px #222126;
+}
+
+span {
+  color: red;
+  margin-bottom: 0.5rem;
 }
 
 h1,
 h2,
 p,
-input::placeholder {
+Field::placeholder {
   color: #222126;
 }
 </style>
